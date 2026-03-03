e3057e1fe9ccbd7291a327076553201518f005a4
Shriti Yadav <shritiyadav12@gmail.com>
2026-03-03 22:19:53 +0530

    Change multer storage to memoryStorage

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -12,6 +12,6 @@ const storage = multer.diskStorage({
 
 
 export const upload = multer({
-    storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
 });
