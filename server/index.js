d60d012a8bd8ce3d9d1a3d7b9dfbf79c8a49f215
Shriti Yadav <shritiyadav12@gmail.com>
2026-03-03 21:19:09 +0530

    Update CORS origin for frontend

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"https://intervuary-backend.onrender.com",
+    origin:"https://intervuary-frontend.onrender.com",
     credentials:true
 }))
 
